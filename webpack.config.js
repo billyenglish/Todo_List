1a51ae30f6a41cf76aaf77562f367e9f55d273a5
Demar English <demarenglish@Mac.attlocal.net>
2024-11-30 22:04:30 -0500

    Adding webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = [
+    "eval",
+].map(devtool => ({
+    mode: "development",
+    entry: {
+        bundle: 
+    }
+}));
