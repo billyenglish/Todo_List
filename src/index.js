dab196ee1b9931ddac285587142e086a93aac174
Billy English <billyenglish6212@outlook.com>
2025-08-12 02:06:47 -0400

    Updated the file for testing purpose.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './style.css';
-import "../src/style.css";
+import './assets/styles/asidebar.css';
+import './assets/styles/main.css';
+import './assets/styles/dialog.css';
+import './assets/styles/mediaquery.css';
+
+const dialog = document.querySelector('#dialog');
