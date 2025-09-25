1540ae0ec9cb18416bb0e91f71599fccf10c29c1
Billy English <billyenglish6212@outlook.com>
2025-09-25 16:03:26 -0400

    Minor update.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import './style.css';
-import './assets/styles/asidebar.css';
-import './assets/styles/main.css';
-import './assets/styles/dialog.css';
-import './assets/styles/mediaquery.css';
 
-const dialog = document.querySelector('#dialog');
 
-dialog.showModal();
