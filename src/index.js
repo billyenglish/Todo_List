bf30c210e952b4b7730d60a67da0c0c53c89e920
Demar English <demarenglish@Mac.attlocal.net>
2025-10-15 17:46:09 -0400

    Inserting the modal features for openning and closing."

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import './assets/styles/asidebar.css';
 import './assets/styles/main.css';
 import './assets/styles/mediaquery.css';
 import './assets/styles/dialog.css';
+import dialogModalDOM from './components/dialogModalDOM';
 
-const dialogForm = document.querySelector('#dialog');
-const createTaskButton = document.querySelector('#create_task_button');
-
-createTaskButton.addEventListener('click', () => {
-    dialogForm.showModal();
-})
+dialogModalDOM();
