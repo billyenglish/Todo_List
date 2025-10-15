65bc23b222a8222147c3763b9d3572c37597e4cc
Billy English <48073749+billyenglish@users.noreply.github.com>
2025-10-15 04:59:29 -0400

    Creating modal interactivity.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './style.css';
+import './assets/styles/asidebar.css';
+import './assets/styles/main.css';
+import './assets/styles/mediaquery.css';
+import './assets/styles/dialog.css';
 
+const dialogForm = document.querySelector('#dialog');
+const createTaskButton = document.querySelector('#create_task_button');
 
+createTaskButton.addEventListener('click', () => {
+    dialogForm.showModal();
+})
