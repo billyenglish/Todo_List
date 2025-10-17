a524dfff380f91027d4dfe9c7c39b1391c6347ca
Billy English <48073749+billyenglish@users.noreply.github.com>
2025-10-17 04:16:50 -0400

    Added the js directory.

---
+++ src/js/components/inboxSection.js
@@ -0,0 +1,43 @@
+const inboxSection = () => {
+    return `
+        <section id="main_section" class="today_section">
+            <header class="header">
+                    <div class="header_container">
+                        <div class="main_section_container">
+                            <i class="ri-menu-unfold-4-line"></i>
+                            <h2 id="header_title">Inbox</h2>
+                        </div>
+
+                        <div>
+                            <ul id="header_menu_list">
+                                <li class="header_list_items">
+                                    <i class="ri-sort-desc"></i>
+                                </li>
+                                <li class="header_list_items second_item_header">
+                                    <i class="fa-solid fa-ellipsis-vertical"></i>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="header_search_container">
+                        <div class="search_container">
+                            <input
+                                type="text"
+                                name="today_search"
+                                class="search_input"
+                                id="today_search"
+                                placeholder="+ Add task"
+                            >
+                            <div class="search_display">
+                                <i class="ri-calendar-event-fill"></i>
+                                <i class="ri-arrow-drop-down-line"></i>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+        </section>
+    `;
+}
+
+export default inboxSection;
